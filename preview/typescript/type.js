c824838ef8f108d6ff9615d91df0b8ef984493dd
lijinfei <1638546595@qq.com>
2022-01-16 09:28:17 +0800

    ts init

---
+++ preview/typescript/type.js
@@ -0,0 +1,8 @@
+var Sex;
+(function (Sex) {
+    Sex[Sex["BOY"] = 0] = "BOY";
+    Sex[Sex["GIRL"] = 1] = "GIRL";
+})(Sex || (Sex = {}));
+console.log(0 /* BOY */, 1 /* GIRL */);
+var a = 1;
+a = null;
---
+++ preview/typescript/type.ts
@@ -0,0 +1,11 @@
+enum Sex {
+  BOY,
+  GIRL,
+}
+const enum S {
+  BOY,
+  GIRL,
+}
+console.log(S.BOY, S.GIRL);
+let a: number = 1;
+a = null;
