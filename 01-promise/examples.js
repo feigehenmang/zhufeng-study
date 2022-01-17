bbb0c6c42613f76c8482379aa06954dc56d4cb90
‘lijinfei’ <lijinfei@winchannel.net>
2022-01-17 18:16:35 +0800

    getNums

--- 01-promise/examples.js
+++ 01-promise/examples.js
@@ -1,4 +1,5 @@
 // nums = [2, 7, 11, 15], target = 9
+//  给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 const nums = [2, 7, 11, 15], target = 9
 // 复杂解法
 function getNums(arr, target) {
@@ -15,7 +16,7 @@ function getNums(arr, target) {
     }
     return result
 }
-
+// 将已遍历的值以 value：index的形式储存，每次循环时查看差值是否存在，存在就返回
 function getNumsTwo(arr, target) {
     const map = {}
     for (let index = 0; index < arr.length; index++) {
