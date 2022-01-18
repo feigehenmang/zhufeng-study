e7f6e581de83d834ccf9c0c4df980cb351efbfba
‘lijinfei’ <lijinfei@winchannel.net>
2022-01-18 17:32:13 +0800

    removeNnode

--- 01-promise/deleteRepeat.js
+++ 01-promise/deleteRepeat.js
@@ -3,15 +3,15 @@ const tree = {
     next: {
         value: 1,
         next: {
-            value: 1,
+            value: 2,
             next: {
-                value: 2,
+                value: 3,
                 next: {
-                    value: 2,
+                    value: 4,
                     next: {
-                        value: 3,
+                        value: 5,
                         next: {
-                            value: 4
+                            value: 6
                         }
                     }
                 }
@@ -19,19 +19,19 @@ const tree = {
         }
     }
 }
-console.log(deletaRepeat(tree))
-function deletaRepeat(queue) {
-    let currHead = queue
-    while (currHead.next) {
-        if (currHead.value === currHead.next.value) {
-            currHead.next = currHead.next.next
-        } else {
-            currHead = currHead.next
-        }
-    }
-    // console.log(JSON.stringify(queue))
-    return queue
-}
+// console.log(deletaRepeat(tree))
+// function deletaRepeat(queue) {
+//     let currHead = queue
+//     while (currHead.next) {
+//         if (currHead.value === currHead.next.value) {
+//             currHead.next = currHead.next.next
+//         } else {
+//             currHead = currHead.next
+//         }
+//     }
+//     // console.log(JSON.stringify(queue))
+//     return queue
+// }
 
 class ListNode {
     constructor(val) {
@@ -39,4 +39,22 @@ class ListNode {
         this.next = null
     }
 }
-
+console.log(removeNnode(tree, 2))
+function removeNnode(queue, n) {
+    const dummyNode = new ListNode()
+    dummyNode.next = queue
+    let start = dummyNode, end = dummyNode
+    // let currHead = queue
+    let i = 1
+    while (i < n) {
+        i++
+        end = end.next
+    }
+    while (end.next) {
+        start = start.next
+        end = end.next
+    }
+    start.next = start.next.next
+    // console.log(JSON.stringify(queue))
+    return queue
+}
