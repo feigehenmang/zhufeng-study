d83c129563fcfb7b2c3b1ebc707d7ba14fc8e647
‘lijinfei’ <lijinfei@winchannel.net>
2022-01-21 15:04:08 +0800

    webpack init

--- webpack-self/src/index.js
+++ webpack-self/src/index.js
@@ -1,12 +1,5 @@
-import './styles/index.css'
-import './styles/index.less'
-import './styles/index.scss'
-
-const a = 10
-document.querySelector('#app').innerHTML = a
-let logo = require('./assets/images/a.jpg');
-let img = new Image();
-console.log(logo)
-img.src = logo;
-document.body.appendChild(img);
-console.log(a)
+export default {
+    add(a, b) {
+        return a + b
+    }
+}
