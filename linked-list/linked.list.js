3e8e8ea5f254e1e75a3d65ac2766479641330895
‘lijinfei’ <lijinfei@winchannel.net>
2022-03-01 14:51:06 +0800

    test

---
+++ linked-list/linked.list.js
@@ -0,0 +1,17 @@
+class Node {
+    constructor(element, next) {
+        this.element = element
+        this.next = next
+    }
+}
+class LinkedList {
+    constructor() {
+        this.size = 0
+        this.head = null
+    }
+
+
+    add(index, element) {
+
+    }
+}
