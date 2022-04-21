be167b1003349b51ce5a7f3bbea49c22ff8aaac4
‘lijinfei’ <lijinfei@winchannel.net>
2022-04-21 10:33:17 +0800

    逆变协变

--- ts/src/func.ts
+++ ts/src/func.ts
@@ -22,3 +22,26 @@ function getValue(v: number | string) {
     v;
   }
 }
+// 简单赋值
+let n1!: number | string
+let n2!: number | string | boolean
+n2 = n1
+type Fn1 = (name: string, age: number) => number
+type Fn2 = (name: string, age: number, address: string) => number
+let fn1!: Fn1
+let fn2!: Fn2
+// 参数少的可以赋值给参数多的
+fn2 = fn1
+
+// 逆变： 函数作为函数参数时 函数执行者是ajax，所以要遵循ajax的参数约束，只能传入number 或者string
+// 此时外部函数定义只能定义包含number string的类型也就是number string的子类型
+// 协变： 函数返回值约束，以传入函数的返回值为准，ajax定义的函数返回值需要是返回值约束类型的父类型
+function ajax(type: 'get' | 'post', cb: (code: number | string) => {code: number|string}) {
+  let obj = cb(200)
+  obj.code
+}
+
+ajax('get', (code: number|string): {code: number|string, age: number} => {
+  console.log(code)
+  return {code, age: 1}
+})
