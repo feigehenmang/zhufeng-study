6035fc4e7b0f9060f9ce76aab91211e8ba2f8747
lijinfei <1638546595@qq.com>
2022-05-02 12:35:11 +0800

    react-window init

---
+++ react-basic/react-project/src/components/ReactWindow.tsx
@@ -0,0 +1,18 @@
+import { FixedSizeList } from 'react-window'
+import './ReactWindow.css'
+const Row = ({ index, style }) => (
+    <div className={index % 2 ? 'ListItemOdd' : 'ListItemEven'} style={style}>Row{index}</div>
+)
+export function ReactWindow() {
+    return (
+        <FixedSizeList
+          className='List'
+          height={200}
+          width={200}
+          itemSize={50}
+          itemCount={1000}
+        >
+            {Row}
+        </FixedSizeList>
+    )
+}
