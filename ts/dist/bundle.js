79b2a2ed3cee7667a6a4ea94156a5462cf6014ef
‘lijinfei’ <lijinfei@winchannel.net>
2022-05-12 16:47:02 +0800

    list

--- ts/dist/bundle.js
+++ ts/dist/bundle.js
@@ -16,28 +16,45 @@
     list2.next.value = 3;
     list2.next.next = new ListNode();
     list2.next.next.value = 4;
-    // console.log(list1, list2)
-    console.log(mergeList(list1, list2));
-    // 单向链表的合并
-    function mergeList(l1, l2) {
-        const r = new ListNode();
-        let cur = r;
-        while (l1 && l2) {
-            // console.log(l1, l2)
-            if (l1.value >= l2.value) {
-                cur.next = l2;
-                l2 = l2.next;
+    const list3 = new ListNode();
+    list3.value = 1;
+    list3.next = new ListNode();
+    list3.next.value = 1;
+    list3.next.next = new ListNode();
+    list3.next.next.value = 1;
+    list3.next.next.next = new ListNode();
+    list3.next.next.next.value = 3;
+    function createList(values) {
+        let list = new ListNode();
+        let curr = list;
+        while (values.length > 0) {
+            curr.value = values[0];
+            values.shift();
+            if (values.length > 0) {
+                curr.next = new ListNode();
+                curr = curr.next;
             }
-            else {
-                cur.next = l1;
-                l1 = l1.next;
-            }
-            cur = cur.next;
         }
-        // console.log(l1, l2)
-        cur.next = (l1 === null || l1 === void 0 ? void 0 : l1.next) ? l1 : l2;
-        return r;
+        return list;
     }
+    createList([1, 1, 2, 2, 3, 4, 5]);
+    // console.log(list4)
+    // console.log(deleteDuplicates(list4))
+    // 删除指定节点
+    createList([1, 2, 3, 4, 5]);
+    // console.log(deleteIndex2(list5, 2))
+    let list6 = createList([1, 2, 3, 4, 5, 6, 7, 8]);
+    function reverseList(list) {
+        let cur = list;
+        let prev;
+        while (cur != null) {
+            cur.next;
+            cur.next = prev;
+            prev = cur;
+        }
+        return prev;
+    }
+    console.log(reverseList(list6));
 
 })();
 //# sourceMappingURL=bundle.js.map
--- ts/src/list2.ts
+++ ts/src/list2.ts
@@ -19,7 +19,7 @@ list2.next.value = 3
 list2.next.next = new ListNode()
 list2.next.next.value = 4
 // console.log(list1, list2)
-console.log(mergeList(list1, list2))
+// console.log(mergeList(list1, list2))
 // 单向链表的合并
 function mergeList(l1?: ListNode, l2?: ListNode): ListNode {
     const r = new ListNode()
@@ -37,7 +37,119 @@ function mergeList(l1?: ListNode, l2?: ListNode): ListNode {
     }
     // console.log(l1, l2)
     cur.next = l1?.next ? l1 : l2
-
     return r
 }
+
+const list3 = new ListNode()
+list3.value = 1
+list3.next = new ListNode()
+list3.next.value = 1
+list3.next.next = new ListNode()
+list3.next.next.value = 1
+list3.next.next.next = new ListNode()
+list3.next.next.next.value = 3
+
+function deleteListNode(list: ListNode) {
+    let cur = list
+    while(cur && cur.next) {
+        // console.log(cur)
+        if(cur.value === cur.next.value) {
+            cur.next = cur.next.next
+        } else {
+            if(cur.next) {
+                cur = cur.next
+            }
+        }
+    }
+    return list
+}
+// console.log(JSON.stringify(list3))
+// console.log(JSON.stringify(deleteListNode(list3)))
+function deleteDuplicates(list: ListNode) {
+    if(!list || !list.next) {
+        return list
+    }
+    const dummy = new ListNode()
+    dummy.next = list
+    let cur = dummy
+    while(cur.next && cur.next.next) {
+        if(cur.next.value === cur.next.next.value) {
+            let val = cur.next.value
+            while(cur.next && cur.next.value === val) {
+                cur.next = cur.next.next
+            }
+        } else {
+            cur = cur.next
+        }
+    }
+    return dummy.next
+}
+function createList(values: any[]) {
+    let list = new ListNode()
+    let curr = list
+    while(values.length > 0) {
+        curr.value = values[0]
+        values.shift()
+        if(values.length > 0) {
+            curr.next = new ListNode()
+            curr = curr.next
+        }
+    }
+    return list
+}
+const list4 = createList([1,1,2,2,3,4,5])
+// console.log(list4)
+// console.log(deleteDuplicates(list4))
+
+// 删除指定节点
+const list5 = createList([1,2,3,4,5])
+
+function deleteIndex(list: ListNode, i: number) {
+    let cur = list, index = 0
+    while(cur && cur.next) {
+        index++
+        cur = cur.next
+    }
+    const deleteIndex = index - i
+    let repeateCurr = list, jndex = 0
+    while(repeateCurr && repeateCurr.next) {
+        if(++jndex === deleteIndex) {
+            if(repeateCurr.next.next) {
+                repeateCurr.next = repeateCurr.next.next
+            }
+        } else {
+            repeateCurr = repeateCurr.next
+        }
+    }
+    return list
+}
+function deleteIndex2(list: ListNode, n: number) {
+    let dummy = new ListNode(), i = 0
+    dummy.next = list
+    let slow = dummy.next, fast = dummy.next
+    while(slow && slow.next && fast && fast.next) {
+        if(++i > n) {
+            slow = slow.next
+        }
+        fast = fast.next
+    }
+    // console.log(slow, fast)
+    slow.next = slow?.next?.next
+    return dummy.next
+}
+// console.log(deleteIndex2(list5, 2))
+
+let list6 = createList([1,2,3,4,5,6,7,8])
+function reverseList(list: ListNode) {
+    let cur = list
+    let prev;
+    while(cur != null) {
+        let next = cur.next
+        cur.next = prev
+        prev = cur
+        cur != next
+    } 
+    return prev
+}
+console.log(reverseList(list6))
 export {}
