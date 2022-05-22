81e35e31872b2d0ebfce729c7454a1c1c0c4d472
lijinfei <1638546595@qq.com>
2022-05-22 21:04:11 +0800

    connect

--- vite/doc/1.connect.js
+++ vite/doc/1.connect.js
@@ -1,7 +1,21 @@
-const connect = require("connect");
+// const connect = require("connect");
 const http = require("http");
 
 const middlewares = connect();
+function connect() {
+  let index = 0;
+  const callbacks = [];
+  const callback = function (req, res) {
+    const next = () => {
+      callbacks[index++](req, res, next);
+    };
+    next();
+  };
+  callback.use = function (fn) {
+    callbacks.push(fn);
+  };
+  return callback;
+}
 middlewares.use(function (req, res, next) {
   console.log("middleware1");
   next();
